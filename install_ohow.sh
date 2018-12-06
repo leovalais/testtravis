d86d9bf4d1765d1f7a2caaa1cf6b4d73c5619b4e
Leo Valais <leo.valais97@gmail.com>
2018-12-06 15:39:00 +0100

    ci: proper opam pin installation of ohow

--- install_ohow.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -e
-
-git clone https://github.com/ocsigen/html_of_wiki
-cd html_of_wiki
-git checkout ohow
-cd -
-make -C html_of_wiki ohow
--- mkdoc.sh
+++ mkdoc.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cp -r doc _doc
-find _doc -name '*.wiki' -exec ./html_of_wiki/ohow.byte {} \; -exec rm {} \;
+find _doc -name '*.wiki' -exec ohow {} \; -exec rm {} \;
