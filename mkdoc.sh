bd8a4911b51ac51391782527d33f2c5c1b1531c6
Leo Valais <leo.valais97@gmail.com>
2018-12-11 11:35:50 +0100

    ci: another approach

--- mkdoc.sh
+++ mkdoc.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
+set -x
+
+wget https://raw.githubusercontent.com/ocaml/ocaml-travisci-skeleton/master/.travis-ocaml.sh
+bash -ex .travis-ocaml.sh
+
+eval $(opam env)
+
+git clone -b ohow --single-branch https://github.com/ocsigen/html_of_wiki.git
+opam pin add -y html_of_wiki html_of_wiki
+
 cp -r doc _doc
 find _doc -name '*.wiki' -exec ohow {} \; -exec rm {} \;
